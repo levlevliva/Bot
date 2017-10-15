94362afcad629a8c5f6f7471942555725ffdd22e
Balázsy Levente <levlevliva@users.noreply.github.com>
2017-10-15 15:10:48 +0200

    Játékkliens addolva.

---
+++ BOT_RFT/BOT_RFT/HttpWeb/RequestMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOT_RFT.HttpWeb
+{
+    public enum RequestMethod
+    {
+        Post,
+        Get,
+    }
+}
